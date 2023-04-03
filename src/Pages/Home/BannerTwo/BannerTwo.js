c4875481b25f88771b69a63deb1a9b42fdb8d992
xbayazid <bayazid9006@gmail.com>
2023-04-04 02:35:50 +0600

    bannerTwo Completed

---
+++ src/Pages/Home/BannerTwo/BannerTwo.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const BannerTwo = () => {
+    return (
+        <div className='lg:flex gap-2 items-center justify-center mx-auto p-10 md:py-20 px-0 md:p-10 md:px-0'>            
+            <div className="content bg-gray-200 p-2 pt-8 md:p-12 pb-12 lg:max-w-lg w-full">
+                <div className=''>
+                    <h2 className='text-3xl'>Essentials starting at $9.99</h2>
+                </div>
+                <button className="mt-2 md:mt-5 p-3 px-5 bg-black text-white font-bold text-sm font-light hover:bg-purple-800">Shop now</button>
+            </div>
+            <div>
+                <img className='xl:max-w-6xl' src="https://images.pexels.com/photos/5990153/pexels-photo-5990153.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1860" alt="" />
+            </div>
+        </div>
+    );
+};
+
+export default BannerTwo;
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Banner from './Banner/Banner';
 import Facilites from './Facilites/Facilites';
 import Category from './Category/Category';
+import BannerTwo from './BannerTwo/BannerTwo';
 
 const Home = () => {
     return (
@@ -9,6 +10,7 @@ const Home = () => {
             <Banner/>
             <Facilites/>
             <Category/>
+            <BannerTwo/>
         </div>
     );
 };
